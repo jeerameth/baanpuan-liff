5bc652bdeb1f4ca41801430f695cc46320e49fb2
jeerameth <klomsinggene@gmail.com>
2022-08-20 22:59:35 +0700

    2 files modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-const plugin = require('tailwindcss/plugin');
 module.exports = {
+  // content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
